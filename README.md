e216af9a549b8ff4ee600cab578c47101452c61e
Evandro Pomatti <evandro.pomatti@hbsis.com.br>
2019-07-11 11:38:03 -0300

    documentation and clean up

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# sentiment-analysis
+# Azure Sentiments
+
 Using Azure Cognitive Services API to identify sentiments within a text.
+
+Runnin the code:
+
+1. Edit and run `azure.ps` file with your preferences
+2. Copy endpoint and keys (from portal) to the code
+3. `npm install`
+4. `node src/index.js`
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const request = require('request');
-const config = require('./config');
 
 const endpoint = 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.1'
+
+// You need to input your own keys
 const key1 = '664b4d4e309848929cdd419d869f95aa'
 const key2 = '56a76e1d661f43879b00c21907203b22'
 
