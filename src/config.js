878b40d367e3a0868457c40eba0dd750ad273b63
Evandro Pomatti <evandro.pomatti@hbsis.com.br>
2019-07-11 11:17:45 -0300

    connected and reading sentiments

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    endpoint: 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.1',
+    key1: '664b4d4e309848929cdd419d869f95aa',
+    key2: '56a76e1d661f43879b00c21907203b22'
+}
+
+module.exports = { config }
---
+++ src/index.js
@@ -0,0 +1,40 @@
+const request = require('request');
+const config = require('./config');
+
+const endpoint = 'https://brazilsouth.api.cognitive.microsoft.com/text/analytics/v2.1'
+const key1 = '664b4d4e309848929cdd419d869f95aa'
+const key2 = '56a76e1d661f43879b00c21907203b22'
+
+headers = {
+    'Ocp-Apim-Subscription-Key': key1,
+    'Content-Type': 'application/json'
+}
+
+function logOutput(err, response, body) {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log(body)
+    }
+}
+
+function detectSentiment(inputText, language) {
+    const body = JSON.stringify({
+        "documents": [
+            {
+                "id": "1",
+                "text": inputText,
+                "language": language
+            }
+        ]
+    })
+    var options = {
+        url: endpoint + '/sentiment',
+        method: 'POST',
+        headers: headers,
+        body: body,
+    }
+    request.post(options, logOutput)
+}
+
+detectSentiment('I am happy', 'en')
